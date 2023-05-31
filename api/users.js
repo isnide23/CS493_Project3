15a2e986321ac806071684756fcbd520323bdff6
root <root@DESKTOP-TKDSS99>
2023-05-31 07:43:56 -0700

    add insertNewUser function

--- api/users.js
+++ api/users.js
@@ -1,5 +1,7 @@
 const { Router } = require('express')
 
+const { validateAgainstSchema } = require('../lib/validation');
+const { generateAuthToken, requireAuthentication } = require('../lib/auth');
 const { Business } = require('../models/business')
 const { Photo } = require('../models/photo')
 const { Review } = require('../models/review')
@@ -29,6 +31,60 @@ router.post('/', async function (req, res, next) {
     });
   }
 })
+
+/*
+ * Route to login.
+ */
+router.post('/login', async function (req, res, next) {
+  if (req.body && req.body.id && req.body.password) {
+    try {
+      const authenticated = await validateUser(req.body.id, req.body.password);
+      if (authenticated) {
+        const token = generateAuthToken(req.body.id);
+        res.status(200).send({
+          token: token
+        });
+      } else {
+        res.status(401).send({
+          error: "Invalid credentials"
+        });
+      }
+    } catch (err) {
+      res.status(500).send({
+        error: "Error validating user.  Try again later."
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body was invalid"
+    });
+  }
+})
+
+/*
+ * Route to get a single user.
+ */
+router.get('/:id', requireAuthentication, async (req, res, next) => {
+  if (req.params.id === req.user) {
+    try {
+      const user = await getUserById(req.params.id);
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error fetching user.  Try again later."
+      });
+    }
+  } else {
+    res.status(403).send({
+      error: "Unauthorized to access the specified resource"
+    });
+  }
+});
 
 /*
  * Route to list all of a user's businesses.
--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize')
 
 const bcrypt = require('bcryptjs');
-const passwordHash = await bcrypt.hash(User.password, 8);
 
 const sequelize = require('../lib/sequelize')
 const { Business} = require('./business')
@@ -9,30 +8,30 @@ const { Photo } = require('./photo')
 const { Review } = require('./review')
 
 const User = sequelize.define('user', {
-  id : { type: DataTypes.INTEGER, allowNull: false },
+  id : { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: true },
   password: { type: DataTypes.STRING, allowNull: false },
-  admin: {type: DataTypes.BOOLEAN, allowNull: false }
+  admin: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
 })
 
 /*
 * Set up one-to-many relationship between User and Business.
 */
-User.hasMany(Business, { foreignKey: { allowNull: false } })
+User.hasMany(Business, { foreignKey: { allowNull: false} })
 Business.belongsTo(User)
 
-/*
-* Set up one-to-many relationship between User and Photo.
-*/
+// /*
+// * Set up one-to-many relationship between User and Photo.
+// */
 User.hasMany(Photo, { foreignKey: { allowNull: false } })
 Photo.belongsTo(User)
 
-/*
-* Set up one-to-many relationship between User and Review.
-*/
-User.hasMany(Review, { foreignKey: { allowNull: false } })
-Review.belongsTo(User)
+// /*
+// * Set up one-to-many relationship between User and Review.
+// */
+// User.hasMany(Review, { foreignKey: { allowNull: false } })
+// Review.belongsTo(User)
 
 exports.User = User
 
@@ -53,12 +52,39 @@ exports.UserClientFields = [
  */
 exports.insertNewUser = async function (user) {
   const userToInsert = extractValidFields(user, UserSchema);
-  const db = getDBReference();
-  const collection = db.collection('users');
 
   const passwordHash = await bcrypt.hash(userToInsert.password, 8);
   userToInsert.password = passwordHash;
 
-  const result = await collection.insertOne(userToInsert);
-  return result.insertedId;
+  const validatedUser = await User.create(userToInsert, UserClientFields)
+
+  return validatedUser;
+};
+
+/*
+ * Fetch a user from the DB based on user ID.
+ */
+async function getUserById(id, includePassword) {
+  const db = getDBReference();
+  const collection = db.collection('users');
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const projection = includePassword ? {} : { password: 0 };
+    const results = await collection
+      .find({ _id: new ObjectId(id) })
+      .project(projection)
+      .toArray();
+    return results[0];
+  }
+};
+exports.getUserById = getUserById;
+
+/*
+ * Fetch a user from the DB based on user ID.
+ */
+exports.validateUser = async function (id, password) {
+  const user = await getUserById(id, true);
+  const authenticated = user && await bcrypt.compare(password, user.password);
+  return authenticated;
 };
